4a1f0ac14b91b8df8fdc7a067a26bd69e801d357
Gustavo Akira Gondo <safegondo@gmail.com>
2020-06-12 16:14:50 -0300

    Initial commit from local code

---
+++ Detectors/OCSVM.py
@@ -0,0 +1,8 @@
+from .meta import IAnomaly
+
+class OCSVMDetector(IAnomaly):
+    def __init__(self, slidingWindowSize = None):
+
+    def detect(self, new_data):
+
+    def detectFromList(self, data):
